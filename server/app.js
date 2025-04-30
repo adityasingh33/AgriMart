b3c3d0593a965f7d8bf3b895be354fde58c16dc9
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2025-04-30 13:30:21 +0530

    Updated cors link

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin : "https://your-frontend-name.vercel.app",
+        origin : "https://agri-mart-bay.vercel.app",
         credentials : true
     })
 );
