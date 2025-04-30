730a11fe4c00a5ddb2a87b65a3b56d07b4346576
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2025-04-30 13:09:38 +0530

    Updated cors link

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin : "http://localhost:5173",
+        origin : "https://your-frontend-name.vercel.app",
         credentials : true
     })
 );
